2da44aeb1f2962e80c7502d3bab7d2d83dd50d58
DangNhanAnh <dang.nhan.anh95@gmail.com>
2017-09-20 11:00:36 +0700

    CSDLSQLB12

---
+++ LTSQLB12.sql
@@ -0,0 +1,34 @@
+﻿USE ONLINE_SHOP
+GO
+
+/*
+ * Viết stored-procedure tính tổng 2 số a, b và in kết quả theo định dạng sau:
+	'Tổng 2 số’ + @a + ‘và’ + @b ‘là:’ + @kq
+ */
+IF OBJECT_ID('uspCau1', 'p') IS NOT NULL
+	DROP PROC uspCau1
+GO
+
+
+ CREATE PROC uspCau1
+	@a float = 0,
+	@b float = 0,
+	@tong float OUT
+AS
+	--RETURN (@a + @b)
+	SET @tong = @a + @b
+GO
+
+--Goi
+
+DECLARE @x float = 1.5,@y float =3.1,@tong float = 0
+EXEC uspCau1 @x, @y, @tong OUT
+PRINT CAST (@x AS varchar)+ ' + '+
+	CAST(@y AS VARCHAR)+' = '+
+	CAST(@tong AS VARCHAR)
+
+
+/*
+ * Viết stored-procedure tính tổng 2 số a, b và in kết quả theo định dạng sau:
+	'Tổng 2 số’ + @a + ‘và’ + @b ‘là:’ + @kq
+ */
